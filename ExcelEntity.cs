043fc1316950e697a0fbce19f29080f9f7ae78fc
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-01 12:14:32 +0700

    SiGG-FinancialTech-DocumentUtility-Quater-ExcelEntity

---
+++ ExcelEntity.cs
@@ -0,0 +1,14 @@
+namespace GM.DocumentUtility
+{
+
+   public class ExcelEntity  
+   {
+      public string ReturnCode = string.Empty;  
+      public string Msg = string.Empty;   
+      public string FileName = string.Empty;  
+      public string PathDes = string.Empty;   
+
+
+      public string SheetName = string.Empty;  
+   }
+}
